98f69a92015a8deaea32bef53649d30ce7cb9741
hankelc <taichenliu@imorpheus.io>
2022-04-10 11:42:55 +0800

    improvement

--- log_process.sh
+++ log_process.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 DATE=$(date +%Y%m%d_%H)
 
-NUM=5
-LOG=log_
+NUM=1
+LOG="./data/log_"
 
 for ((i=0;i<${NUM};i++))
 do
     LOG_FILE=${LOG}${i}
-    tar -zcvf ./data/${LOG_FILE}_${DATE}.tar.gz ./data/${LOG_FILE}
+    tar -zcvf ${LOG_FILE}_${DATE}.tar.gz ${LOG_FILE}
 done
 
-RMDATE=$(date "+%Y%m%d_%H" -d "-3 days")
+RMDATE=$(date "+%Y%m%d_%H" -d "-1 days")
 
-TAR_BAK="./data/log_*_$RMDATE.tar.gz"
+TAR_BAK="./log_*_$RMDATE.tar.gz"
 
 rm -rf $TAR_BAK
